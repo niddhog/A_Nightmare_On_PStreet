2e6d8eaa4ea9f0014ddfd08d80f904d14b19915b
ChristianAeberhard <christian.aeberhard2@uzh.ch>
2020-12-25 09:23:12 +0100

    EnemySpawnScript
    Setting up the Class responsible for spawning enemies

--- Assets/Scripts/EnemySpawnScript.cs
+++ Assets/Scripts/EnemySpawnScript.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawScript : MonoBehaviour
+public class EnemySpawnScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject zombie;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
